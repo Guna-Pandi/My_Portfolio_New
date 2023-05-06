eb912b251d286a9666bcf95793b6e7dec0e15a83
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-07 00:03:50 +0530

    Added:loader

--- src/App.js
+++ src/App.js
@@ -8,9 +8,40 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  // //Disable Right Click
+  // useEffect(() => {
+  //   const handleContextmenu = (e) => {
+  //     e.preventDefault();
+  //   };
+  //   document.addEventListener("contextmenu", handleContextmenu);
+  //   return function cleanup() {
+  //     document.removeEventListener("contextmenu", handleContextmenu);
+  //   };
+  // }, []);
+
   return (
+    <div>
+    { loading?
+    <div className="loader__symbol">
+      <PacmanLoader
+        color={ '#4db5ff'}
+        loading={loading}
+        size={100}
+      />
+    </div>
+      :
     <>
       <Header />
       <Nav />
@@ -21,6 +52,8 @@ function App() {
       <Contact />
       <Footer />
     </>
+}
+</div>
   );
 }
 
