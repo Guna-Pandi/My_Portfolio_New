2c388371ec8849b8f97a929753f63d81b4823637
Guna P <111977753+Guna-Pandi@users.noreply.github.com>
2023-05-21 00:34:01 +0530

    Fix :Contacts

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -23,42 +23,48 @@ const Contact = () => {
 
   return (
     <section id="contact">
-      <motion.h1 className="contact__h1" initial="hidden"
+      <motion.h1
+        className="contact__h1"
+        initial="hidden"
         whileInView="visible"
         viewport={{ once: false, amount: 0.3 }}
         transition={{ duration: 0.5 }}
         variants={{
           hidden: { opacity: 0, x: -30 },
           visible: { opacity: 1, x: 0 },
-        }}>Contact</motion.h1>
+        }}>
+        Contact
+      </motion.h1>
       <h5 className="contact__h5">Get In Touch</h5>
 
       <div className="container contact__container">
         <div className="contact__options">
-          <motion.aritcle className="contact__option"
-           initial="hidden"
-              whileInView="visible"
-              viewport={{ once: false, amount: 0.6 }}
-              transition={{ duration: 0.7 }}
-              variants={{
-                hidden: { opacity: 0, y: -80 },
-                visible: { opacity: 1, y: 0 },
-              }}>
+          <motion.aritcle
+            className="contact__option"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.6 }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, y: -80 },
+              visible: { opacity: 1, y: 0 },
+            }}>
             <TbMailForward className="contact__option-icon" />
             <h4>Email</h4>
             <h5>gunapandidurai@gmail.com</h5>
             <a href="mailto:gunapandidurai@gmail.com">Send a Message </a>
           </motion.aritcle>
 
-          <motion.aritcle className="contact__option"
-           initial="hidden"
-              whileInView="visible"
-              viewport={{ once: false, amount: 0.6 }}
-              transition={{ duration: 0.7 }}
-              variants={{
-                hidden: { opacity: 0, y: -80 },
-                visible: { opacity: 1, y: 0 },
-              }}>
+          <motion.aritcle
+            className="contact__option"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.6 }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, y: -80 },
+              visible: { opacity: 1, y: 0 },
+            }}>
             <BsInstagram className="contact__option-icon" />
             <h4>Instagram</h4>
             <h5>guna3437</h5>
@@ -67,15 +73,16 @@ const Contact = () => {
             </a>
           </motion.aritcle>
 
-          <motion.aritcle className="contact__option"
-           initial="hidden"
-              whileInView="visible"
-              viewport={{ once: false, amount: 0.5 }}
-              transition={{ duration: 0.6 }}
-              variants={{
-                hidden: { opacity: 0, y: -80 },
-                visible: { opacity: 1, y: 0 },
-              }}>
+          <motion.aritcle
+            className="contact__option"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.6 }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, y: -80 },
+              visible: { opacity: 1, y: 0 },
+            }}>
             <BsWhatsapp className="contact__option-icon" />
             <h4>Whatsapp</h4>
             <a href="https://wa.me/qr/Y54UPQ7GNAJ5P1">Send a Message </a>
@@ -100,15 +107,17 @@ const Contact = () => {
             rows="7"
             required
             placeholder="Your Message 📝"></textarea>
-          <motion.button type="submit" className="btn btn-primary"
-           initial="hidden"
-              whileInView="visible"
-              viewport={{ once: false, amount: 0.5 }}
-              transition={{ duration: 0.7 }}
-              variants={{
-                hidden: { opacity: 0, x: -80 },
-                visible: { opacity: 1, x: 0 },
-              }}>
+          <motion.button
+            type="submit"
+            className="btn btn-primary"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.7 }}
+            variants={{
+              hidden: { opacity: 0, x: -80 },
+              visible: { opacity: 1, x: 0 },
+            }}>
             Send Message 📩
           </motion.button>
         </form>
